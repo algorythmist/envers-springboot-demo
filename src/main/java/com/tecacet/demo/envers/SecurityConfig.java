b3ba68f14cf177f4945c162e94ac7267df4adce1
algorythmist <dimitri@tecacet.com>
2023-01-16 20:56:35 +0000

    disabled security

---
+++ src/main/java/com/tecacet/demo/envers/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.tecacet.demo.envers;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  {
+
+    @Bean
+    SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.antMatcher("/")
+                .httpBasic().disable();
+        return httpSecurity.build();
+    }
+
+}
